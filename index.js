d9667c04a09ce413c82430d36c5c648c3cee531d
Kamil Mikosz <kamil.mikosz@xsolve.pl>
2018-03-09 15:51:58 +0100

    Repair exports

---
+++ index.js
@@ -0,0 +1,19 @@
+const CART = require('./lib/algorithms/CART');
+const ID3 = require('./lib/algorithms/ID3');
+const Algorithm = require('./lib/structure/algorithm');
+const Leaf = require('./lib/structure/leaf');
+const Node = require('./lib/structure/node');
+const Tree = require('./lib/structure/tree');
+const Indicators = require('./lib/qualityIndicators');
+const TreeLoader = require('./lib/utils/treeLoader');
+
+module.exports(
+    CART,
+    ID3,
+    TreeLoader,
+    Algorithm,
+    Leaf,
+    Node,
+    Tree,
+    Indicators,
+);
