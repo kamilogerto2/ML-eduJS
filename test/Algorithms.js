7f47f8a570b92891c00a4022a7c51551d7e4263d
Kamil Mikosz <kamil.mikosz@xsolve.pl>
2018-03-09 11:43:17 +0100

    Init CART and ID3 method

---
+++ test/Algorithms.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const CART = require('../lib/algorithms/CART');
+const ID3 = require('../lib/algorithms/ID3');
+
+const learningData = [
+    ['Green', 3, 'Apple'],
+    ['Yellow', 3, 'Apple'],
+    ['Red', 1, 'Grape'],
+    ['Red', 1, 'Grape'],
+    ['Yellow', 3, 'Lemon'],
+];
+const labels = ['colors', 'number'];
+
+
+describe('Algorithms', function() {
+    describe('CART', function() {
+        const cartAlgorithm = new CART();
+
+        it('algorithm object should be created', function() {
+            assert.equal(cartAlgorithm.type, 'CART');
+        });
+
+        it('tree should be built without errors', function() {
+            assert.equal(typeof cartAlgorithm.buildTree(learningData, labels), 'object');
+        });
+
+        it('tree should predict correct data', function() {
+            tree = cartAlgorithm.buildTree(learningData, labels);
+            assert.equal(tree.classify(['Red', 1, 'Grape'])[0].hasOwnProperty('Grape'), true);
+        });
+    });
+
+    describe('ID3', function() {
+        const id3Algorithm = new ID3();
+
+        it('algorithm object should be created', function() {
+            assert.equal(id3Algorithm.type, 'ID3');
+        });
+
+        it('tree should be built without errors', function() {
+            assert.equal(typeof id3Algorithm.buildTree(learningData, labels), 'object');
+        });
+
+        it('tree should predict correct data', function() {
+            tree = id3Algorithm.buildTree(learningData, labels);
+            assert.equal(tree.classify(['Red', 1, 'Grape'])[0].hasOwnProperty('Grape'), true);
+        });
+    });
+});
