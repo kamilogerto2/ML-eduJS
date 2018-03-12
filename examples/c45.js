3295f3d7404e9b1f6bac22a38e4e3f31779ce4d8
Kamil Mikosz <kamil.mikosz@xsolve.pl>
2018-03-12 12:53:56 +0100

    add continues values into C4.5 (wip)

--- examples/c45.js
+++ examples/c45.js
@@ -2,10 +2,13 @@ const C45 = require('../lib/algorithms/C45');
 
 // define learning data
 const learningData = [
-    ['Green', 'Sweet', 'Apple'],
-    ['Orange', 'Bitter', 'Grape'],
-    ['Orange', 'Sweet', 'Orange'],
-    ['Purple', 'Bitter', 'Plum'],
+    ['Green', 8, 'Apple'],
+    ['Orange', 1, 'Grape'],
+    ['Orange', 6, 'Orange'],
+    ['Purple', 7, 'Plum'],
+    ['Red', 5, 'Orange'],
+    ['Red', 7, 'Orange'],
+    ['Red', 9, 'Apple'],
 ];
 
 //define feature labels
@@ -15,7 +18,7 @@ const labels = {
         order: 0
     },
     taste: {
-        type: 'string',
+        type: 'numeric',
         order: 1
     }
 };
@@ -26,6 +29,6 @@ const cartAlgorithm = new C45(descriptiveMode);
 const tree = cartAlgorithm.buildTree(learningData, labels);
 
 // check it ;)
-const prediction = tree.classify(['Purple', 'Bitter', 'Plum']);
+const prediction = tree.classify(['Red', 8, 'Apple']);
 console.log(`\n Prediction for our test sample:`);
 console.log(prediction);
