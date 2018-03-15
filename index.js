12ec7ca98e83e4bd7330dbbc4ff05e08f4800836
Kamil Mikosz <kamil.mikosz@xsolve.pl>
2018-03-15 11:53:58 +0100

    update index.js with new algorithms

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const CART = require('./lib/algorithms/CART');
 const ID3 = require('./lib/algorithms/ID3');
+const C45 = require('./lib/algorithms/C45');
+const Bagging = require('./lib/algorithms/bagging');
 const Algorithm = require('./lib/structure/algorithm');
 const Leaf = require('./lib/structure/leaf');
 const Node = require('./lib/structure/node');
 const Tree = require('./lib/structure/tree');
+const TreeBag = require('./lib/structure/treeBag');
 const Indicators = require('./lib/qualityIndicators');
 const TreeLoader = require('./lib/utils/treeLoader');
+const BinaryModelMetrics = require('./lib/utils/binaryModelMetrics');
 
 module.exports = {
     CART,
     ID3,
+    C45,
+    Bagging,
+    TreeBag,
+    BinaryModelMetrics,
     TreeLoader,
     Algorithm,
     Leaf,
